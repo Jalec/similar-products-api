21cc7cc54662db0b7f205fe48ecf5e3b72f6c3ea
Jordi <jordi.alejandre10@gmail.com>
2025-03-23 12:31:59 +0100

    first commit

---
+++ similar-products-api/src/test/java/com/example/similar_products_api/SimilarProductsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.similar_products_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilarProductsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
